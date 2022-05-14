5f1f59c3d95dcaab17c7c52c34966a0344279f53
Michael <michaelkmb@hotmail.com>
2022-05-14 16:31:34 -0300

    JS: setInterval(), setTimeout();

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
-/*
- * 
- * < 17: muito abaixo do peso
- * < 18,5: abaixo do peso
- * < 25: normal
- * > 25: acima do peso
- * 
- * peso/altura**2
- */
-
-const imcTable = {  0: 'Muito abaixo do peso',
-                    17: 'Abaixo do peso',
-                    18.5: 'Peso ideal',
-                    25: 'Acima do peso',
-                };
-
-let peso = document.getElementById('peso').value;
-let altura = document.getElementById('altura').value;
-
-function calcImc(event) {
-    event.preventDefault();
-    let imc = (peso/(altura*altura)).toFixed(1);
-    document.getElementById('resultado').innerHTML = imc;
+function toggleDarkTheme(){
+    var b = document.body;
+    b.classList.toggle('dark-theme');
 }
 
+var toggle = setInterval(toggleDarkTheme, 2000);
+clearInterval(toggle);
+
+setTimeout(() => {
+    var b = document.body;
+    b.classList.toggle('dark-theme');
+}, 5000);
