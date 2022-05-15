67c5f214862262866bb7c7aa901d2a865aadd0e9
Michael <michaelkmb@hotmail.com>
2022-05-15 17:05:06 -0300

    first useState n use parameter;

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
+//custom components
 import Welcome from './components/Welcome.js';
 
+
 function App() {
+  const [nome, setNome] = useState('Michael');
+
+  function changeName(newName){
+    setNome(newName);
+  }
+
   return (
     <div>
       <h1>Este é meu projeto</h1>
-      <Welcome nome="Michael"/>
-      <br/>
-      <Welcome nome="Andrei"/>
+      <h2>Olá: {nome}</h2>
+      <button onClick={ () => changeName('Teste') }>
+        Mudar nome
+      </button>
     </div>
   );
 }
