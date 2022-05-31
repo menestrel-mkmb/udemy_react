b82dcc92957c4743bd5d2062f81d7ba63e6601d3
Michael <michaelkmb@hotmail.com>
2022-05-31 14:11:10 -0300

    Nutri to App;

--- segundo-app/src/App.js
+++ segundo-app/src/App.js
@@ -1,48 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
-const API_URL = "https://sujeitoprogramador.com/rn-api/?api=posts";
+import Nutri from "./components/Nutri.js";
 
 function App() {
-  const [nutriNl, setNutriNl] = useState([]);
-
-  useEffect(() => {
-    function loadApi() {
-      fetch(API_URL)
-        .then((response) => response.json())
-        .then((json) => setNutriNl(json));
-    }
-
-    loadApi();
-  }, [nutriNl]);
-
-  return (
-    <div className="App parent__row">
-      <header className="header__sect sect expanded">
-        <h1 className="page__title title">Nutri NewsLetter | React</h1>
-      </header>
-      <main className="main__sect content sect ">
-        {nutriNl.map((newl) => {
-          return (
-            <article
-              key={newl.id}
-              className="parent__row item new__item new__artc"
-            >
-              <a href="#">
-                <h2 className="title new__title title-item">{newl.titulo}</h2>
-                <div className="parent__col">
-                  <img src={newl.capa} className="new__img img thumb"></img>
-                  <p className="sub-title new__sub-title sub-title-item">
-                    {newl.subtitulo}
-                  </p>
-                </div>
-              </a>
-            </article>
-          );
-        })}
-      </main>
-    </div>
-  );
+  return <Nutri />;
 }
 
 export default App;
