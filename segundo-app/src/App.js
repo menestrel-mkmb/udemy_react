47bcd498694e89931ebf6f2bcddb1cb76107ddfe
Michael <michaelkmb@hotmail.com>
2022-05-31 10:09:59 -0300

    dynList Nutri News; P style Nutri News;

--- segundo-app/src/App.js
+++ segundo-app/src/App.js
@@ -4,11 +4,43 @@ import "./App.css";
 const API_URL = "https://sujeitoprogramador.com/rn-api/?api=posts";
 
 function App() {
+  const [nutriNl, setNutriNl] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      fetch(API_URL)
+        .then((response) => response.json())
+        .then((json) => setNutriNl(json));
+    }
+
+    loadApi();
+  }, [nutriNl]);
+
   return (
-    <div className="App">
-      <header className="header__sect container sect">
+    <div className="App parent__row">
+      <header className="header__sect sect expanded">
         <h1 className="page__title title">Nutri NewsLetter | React</h1>
       </header>
+      <main className="main__sect content sect ">
+        {nutriNl.map((newl) => {
+          return (
+            <article
+              key={newl.id}
+              className="parent__row item new__item new__artc"
+            >
+              <a href="#">
+                <h2 className="title new__title title-item">{newl.titulo}</h2>
+                <div className="parent__col">
+                  <img src={newl.capa} className="new__img img thumb"></img>
+                  <p className="sub-title new__sub-title sub-title-item">
+                    {newl.subtitulo}
+                  </p>
+                </div>
+              </a>
+            </article>
+          );
+        })}
+      </main>
     </div>
   );
 }
