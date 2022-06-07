ce0425d9aeb1b01a50a7e54b8f254dbe6cb5c3fe
Michael <michaelkmb@hotmail.com>
2022-06-07 14:54:34 -0300

    link to specific movie id;

--- 03 - prime-flix/src/pages/Home/index.js
+++ 03 - prime-flix/src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { API_PARAMS, api, endpoints } from "../../services/API";
+import { Link } from "react-router-dom";
 
-import "./styles.css";
+import "./../../reset.css";
+import "./home.css";
 
 const index_param = {
   api_key: API_PARAMS.KEY,
@@ -27,18 +29,19 @@ function Home() {
   return (
     <main className="main__sect content parent_row">
       <section className="movies__sect now-playing__list list">
-        {filmes.map((value) => {
+        {filmes.map((movie) => {
           return (
-            <article key={value.id} className="movie item movie__artc article">
-              <h2 className="movie__title"> {value.title} </h2>
+            <article key={movie.id} className="movie item movie__artc article">
+              <h2 className="movie__title title"> {movie.title} </h2>
               <img
-                src={endpoints.images_url_base + value.poster_path}
-                alt=""
+                src={endpoints.images_url_base + movie.poster_path}
+                alt={"poster " + movie.title}
                 className="banner__movie banner img"
               />
               <span className="desc text desc__movie overview">
-                {value.overview}
+                {movie.overview}
               </span>
+              <Link to={"/movie/" + movie.id}>Acessar</Link>
             </article>
           );
         })}
