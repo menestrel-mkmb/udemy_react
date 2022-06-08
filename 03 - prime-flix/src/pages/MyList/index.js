4e36aea184cdd2bab59be37283003e83e77dfc82
Michael <michaelkmb@hotmail.com>
2022-06-08 09:01:25 -0300

    movieList localStorage;

--- 03 - prime-flix/src/pages/MyList/index.js
+++ 03 - prime-flix/src/pages/MyList/index.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+
 function MyList() {
+  let [myList, setMyList] = useState([]);
+  let localList;
+
+  useEffect(() => {
+    localList = localStorage.getItem("@myList");
+  }, []);
+
+  useEffect(() => {
+    localList.setItem("@myList", localList);
+  }, myList);
+
+  function removeMovie(e, movieToRemove) {
+    e.preventDefault();
+
+    setMyList((myList) => {
+      myList.filter((movie) => {
+        return movie.id != movieToRemove;
+      });
+    });
+  }
+
   return (
-    <main>
-      <section></section>
+    <main className="main__sect content__sect content">
+      <section className="my-list__sect container flex parent_col">
+        {myList.map((movie) => {
+          return (
+            <article
+              key={movie.id}
+              className="movie__artc article movie__item item list__item"
+            >
+              <h2 className="movie__title title movie__name">{movie.title}</h2>
+              <form action="" onSubmit={removeMovie(e, movie.id)}>
+                <button type="submit"> - </button>
+              </form>
+            </article>
+          );
+        })}
+      </section>
     </main>
   );
 }
